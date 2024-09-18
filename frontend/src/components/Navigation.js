5aed4f0a0f78121a927627c8e6ba78498244a8cb
Sam V <s4638535@student.uq.edu.au>
2024-09-18 21:02:52 +1000

    Fix navigation bar size

--- frontend/src/components/Navigation.js
+++ frontend/src/components/Navigation.js
@@ -4,11 +4,14 @@ import './styles.css';  // 确保导入了正确的 CSS 文件
 
 const Navigation = () => {
     return (
-        <nav className="navigation">
-            <NavLink to="/home" activeClassName="active">🏠</NavLink>
-            <NavLink to="/events" activeClassName="active">📅</NavLink>
-            <NavLink to="/profile" activeClassName="active">📁</NavLink>
-        </nav>
+        <div>
+            <nav className="navigation">
+                <NavLink to="/home" activeClassName="active">🏠</NavLink>
+                <NavLink to="/events" activeClassName="active">📅</NavLink>
+                <NavLink to="/profile" activeClassName="active">📁</NavLink>
+            </nav>
+        </div>
+
     );
 };
 
